84d226de3652105ecb9f4afbca09c3622761a907
mingsung-dev <mingsung521@gmail.com>
2021-01-13 23:50:42 +0900

    디미고 파이썬 문제풀이

---
+++ pythonProject/codes/4.4.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+a = int(input())
+b = int(input())
+
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+def mul(a,b):
+	return a*b
+
+def div(a,b):
+	return a/b
+
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
