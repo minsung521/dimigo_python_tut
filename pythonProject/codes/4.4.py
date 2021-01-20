90ef000b865e9e67b2f40e782a27de84486fed14
mingsung-dev <mingsung521@gmail.com>
2021-01-20 23:59:29 +0900

    swap

--- pythonProject/codes/4.4.py
+++ pythonProject/codes/4.4.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
 
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(input())  # 숫자1
+b = int(input())  # 숫자2
 
 
-def my_max(a, b, c):
-    return a if a > b and a > c else (b if b > c else c)
+def swap(a, b):
+    return b, a
 
 
-print(my_max(a, b, c))
+a, b = swap(a, b)
+
+print(a, b)  # 실행 전에 미리 값을 예상해 보세요.
