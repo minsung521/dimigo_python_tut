c71804273d1ae675a02c1eac55e38fdc2fce3416
mingsung-dev <mingsung521@gmail.com>
2021-01-23 23:25:53 +0900

    str slicing

--- pythonProject/codes/4.4.py
+++ pythonProject/codes/4.4.py
@@ -1,19 +1,8 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
 
-my_list = ['a', 1, 2, 3, 'b', ['apple', 'banana'], 4]
+num = input()
 
-# [1, 2]을 출력하세요
-print(my_list[1:3])
-
-# [3, 'b']를 출력하세요
-print(my_list[3:5])
-
-# [1, 2, 3, 'b']를 출력하세요
-print(my_list[1:5])
-
-# [4]를 출력하세요
-print(my_list[6:])
-
-# ['a', 3, 4]를 출력하세요
-print(my_list[::3])
+# YYMMDD-DDDDDDD 형식
+print(num[:6])
+print(num[7:])
