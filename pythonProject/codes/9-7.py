0b9172542bb7992bd50d0dc0d1252afc989e93c4
mingsung-dev <mingsung521@gmail.com>
2021-02-08 23:33:44 +0900

    과일가게 2

--- pythonProject/codes/9-7.py
+++ pythonProject/codes/9-7.py
@@ -2,14 +2,17 @@
 # UTF-8 encoding when using korean
 
 apple = int(input())  # 사과 수
+apple_price = int(input())  # 사과 단가
 banana = int(input())  # 바나나 수
+banana_price = int(input())  # 바나나 단가
 kiwi = int(input())  # 키위 수
+kiwi_price = int(input())  # 키위 단가
 
-fruits = {"apple": apple, "banana": banana, "kiwi": kiwi}
+dic = {"apple": (apple, apple_price), "banana": (banana, banana_price), "kiwi": (kiwi, kiwi_price)}
 
 total = 0
 
-for i in fruits.values():
-    total = total + i
+for i in dic.keys():
+    total = total + dic[i][0] * dic[i][1]
 
 print(total)
