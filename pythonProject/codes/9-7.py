8041bb8bd539a166c6df4463de04afaf49a38504
mingsung-dev <mingsung521@gmail.com>
2021-02-10 23:23:34 +0900

    open files

--- pythonProject/codes/9-7.py
+++ pythonProject/codes/9-7.py
@@ -1,18 +1,6 @@
-# -*- coding: utf-8 -*-
-# UTF-8 encoding when using korean
+f = open("../files/test.txt", 'w')  # 파일 쓰기모드로 열기
 
-apple = int(input())  # 사과 수
-apple_price = int(input())  # 사과 단가
-banana = int(input())  # 바나나 수
-banana_price = int(input())  # 바나나 단가
-kiwi = int(input())  # 키위 수
-kiwi_price = int(input())  # 키위 단가
+f.write('pizza\n')
+f.write('pasta\n')
 
-dic = {"apple": (apple, apple_price), "banana": (banana, banana_price), "kiwi": (kiwi, kiwi_price)}
-
-total = 0
-
-for i in dic.keys():
-    total = total + dic[i][0] * dic[i][1]
-
-print(total)
+f.close()  # 파일 닫기
